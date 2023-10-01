a77027a7d397bbb260d153ae13a0f7a84ecccb05
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-10-01 13:24:34 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import UserForm from './UserForm';
+import UserList from './UserList';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
   );
 }
---
+++ src/UserForm.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function UserForm({ onUserAdd }) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onUserAdd({ name, email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
+
+export default UserForm;
