c3540e5c5cf9eb85565da81dc4ba69e8934e3c57
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-10-07 17:30:42 +0330

    feat (user) : update UserList.test

--- src/User/UserList.test.js
+++ src/User/UserList.test.js
@@ -1,3 +1,4 @@
+
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 test ('render one row per user',()=>{
@@ -9,6 +10,23 @@ test ('render one row per user',()=>{
    expect(rows).toHaveLength(2);
 })
 test ('render the email and name of each user',()=>{
+    const users=[{name:'jane',email:'jane@jane.com'},
+    {name:'sam',email:'sam@sam.com'}
+]
+    render(<UserList users={users} />)
+    for( let user of users){
+        const name=screen.getByRole('cell',{name:user.name})
+        const email=screen.getByRole('cell',{name:user.email})
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
 
+    }
 })
+
+
+
+
+
+    // screen.logTestingPlaygroundURL()
+
 
