e7c9146cdc593b13bf392cd9e6f7648b2ed32542
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-10-08 16:48:19 +0330

    feat (Modal) : add user choice test

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -6,21 +6,25 @@ export default function Modal (){
 
   const handleAccept = () => {
     setUserChoice('accept');
+    setShowModal(prev=>!prev)
   };
 
   const handleDecline = () => {
     setUserChoice('decline');
+    setShowModal(prev=>!prev)
   };
     return(
         <>
         
 
-<div className="flex justify-center pt-44" >
+<div className="flex items-center pt-44 flex-col " >
 <button 
 onClick={()=>setShowModal(prev=>!prev)}
-data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+data-modal-target="defaultModal" data-modal-toggle="defaultModal" class=" w-40 mb-8 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
   Open modal
 </button>
+<h1 data-testid="userChoice" >{userChoice}</h1>
+
 </div>
 
 {showModal &&
--- src/Modal/Modal.test.js
+++ src/Modal/Modal.test.js
@@ -4,10 +4,7 @@ import Modal from './Modal';
 
 test('When I click on the button the modal is displayed', () => {
   render(<Modal />);
-  
   const openModalButton = screen.getByRole('button', { name: /open modal/i });
-
-
   fireEvent.click(openModalButton);
   const modalTitle =  screen.getByRole('heading',{ name: /Terms of Service/i });
   expect(modalTitle).toBeInTheDocument();
@@ -16,11 +13,7 @@ test('When I click on the button the modal is displayed', () => {
 
 test('Modal closes when the close button is clicked', () => {
   render(<Modal />);
-  
   const openModalButton = screen.getByRole('button', { name: /open modal/i });
-
-
- 
   fireEvent.click(openModalButton);
 const closeButton = screen.getByTestId('CloseModal')
 
@@ -29,4 +22,47 @@ const closedModal = screen.queryByTestId('CloseModal');
 // expect(closedModal).toBeNull();دو  مچر برابر 
 expect(closedModal).not.toBeInTheDocument();
  
+});
+
+
+
+test('User can accept  the terms', () => {
+
+
+  render(<Modal />);
+  const openModalButton = screen.getByRole('button', { name: /open modal/i });
+  fireEvent.click(openModalButton);
+  // User accepts the terms
+  const AcceptButton = screen.getByRole('button', { name: /i accept/i });
+
+  expect(AcceptButton).toBeInTheDocument();
+  fireEvent.click(AcceptButton);
+  const userChoice =  screen.getByTestId('userChoice');
+  expect(userChoice).toBeInTheDocument();
+  expect(userChoice).toHaveValue('accept');
+
+
+});
+
+
+
+
+test('User can  decline the terms', () => {
+
+  render(<Modal />);
+  const openModalButton = screen.getByRole('button', { name: /open modal/i });
+  fireEvent.click(openModalButton);
+  // User accepts the terms
+  const declineButton = screen.getByRole('button', { name: /decline/i });
+
+  expect(declineButton).toBeInTheDocument();
+  fireEvent.click(declineButton);
+  const userChoice =  screen.getByTestId('userChoice');
+  expect(userChoice).toBeInTheDocument();
+
+
+
+ 
+
+
 });
