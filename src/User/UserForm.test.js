2497699dc691e0f6c8d7bef1d2cc5bad96735a29
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-10-08 10:32:01 +0330

    feat (user) : update userform test

--- src/User/UserForm.test.js
+++ src/User/UserForm.test.js
@@ -47,3 +47,21 @@ test('it calls onUserAdd when the form is submitted', () => {
   expect(mock).toHaveBeenCalled();
   expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com' });
 });
+
+test('empties the two inputs when form is submitted', () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  user.click(nameInput);
+  user.keyboard('jane');
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
+
+  user.click(button);
+
+  expect(nameInput).toHaveValue('');
+  expect(emailInput).toHaveValue('');
+});
