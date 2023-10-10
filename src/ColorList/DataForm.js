dbdfc31f62dd1c716f19078e0b2753e945b20de1
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-10-10 11:38:53 +0330

    feat (*) : add DataForm

---
+++ src/ColorList/DataForm.js
@@ -0,0 +1,31 @@
+import { screen, render } from '@testing-library/react';
+import { useState } from 'react';
+
+export default function DataForm() {
+  const [email, setEmail] = useState('asdf@asdf.com');
+
+  return (
+    <form>
+      <h3>Enter Data</h3>
+
+      <div data-testid="image wrapper">
+        <img alt="data" src="data.jpg" />
+      </div>
+
+      <label htmlFor="email">Email</label>
+      <input 
+        id="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <label htmlFor="color">Color</label>
+      <input id="color" placeholder="Red" />
+
+      <button title="Click when ready to submit" data-testid="submit-button">
+        Submit
+      </button>
+    </form>
+  );
+}
+
---
+++ src/ColorList/DataForm.test.js
@@ -0,0 +1,23 @@
+import { render, fireEvent,screen } from '@testing-library/react';
+import DataForm from './DataForm';
+
+test('selecting different elements', () => {
+    render(<DataForm />);
+  
+    const elements = [
+      screen.getByRole('button'),
+      screen.getByText(/enter/i),
+  
+      screen.getByLabelText(/email/i),
+      screen.getByPlaceholderText('Red'),
+      screen.getByDisplayValue('asdf@asdf.com'),
+      screen.getByAltText('data'),
+      screen.getByTitle(/ready to submit/i),
+  
+      screen.getByTestId('image wrapper')
+    ];
+  
+    for (let element of elements) {
+      expect(element).toBeInTheDocument();
+    }
+  });
